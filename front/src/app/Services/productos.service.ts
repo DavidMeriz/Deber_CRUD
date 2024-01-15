e9b262ed705687921cab571effdaf76ae5823f3b
DavidMeriz <davidecheverria1028@hotmail.com>
2024-01-14 21:17:38 -0500

    first commit

---
+++ front/src/app/Services/productos.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProductos } from '../Interfaces/iproducto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductosService {
+  private urlBase: string =
+    'http://localhost/Deber_CRUD/Inventario/Controllers/Producto.Controller.php?op=';
+  constructor(private cliente: HttpClient) {}
+
+  todos(): Observable<IProductos[]> {
+    return this.cliente.get<IProductos[]>(this.urlBase + 'todos');
+  }
+  uno(id: number): Observable<IProductos> {
+    var prod = new FormData();
+    prod.append('productoId', id.toString());
+    return this.cliente.post<IProductos>(this.urlBase + 'uno', prod);
+  }
+  insertar(producto: IProductos): Observable<any> {
+    var prod = new FormData();
+    prod.append('Nombre', producto.Nombre);
+    prod.append('Precio', producto.Precio.toString());
+    prod.append('Cantidad', producto.Cantidad.toString());
+    return this.cliente.post(this.urlBase + 'insertar', prod);
+  }
+  actualizar(producto: IProductos, id: number): Observable<any> {
+    var prod = new FormData();
+    prod.append('productoId', id.toString());
+    prod.append('Nombre', producto.Nombre);
+    prod.append('Precio', producto.Precio.toString());
+    prod.append('Cantidad', producto.Cantidad.toString());
+    return this.cliente.post(this.urlBase + 'actualizar', prod);
+  }
+  eliminar(id: number): Observable<any> {
+    var prod = new FormData();
+    prod.append('productoId', id.toString());
+    return this.cliente.post(this.urlBase + 'eliminar', prod);
+  }
+}
+
---
+++ front/src/app/Services/stock.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IStock } from '../Interfaces/istock';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+  private urlBase: String = 'http://localhost/Deber_CRUD/Inventario/Controllers/Stock.Controller.php?op='
+  constructor(private cliente: HttpClient) { }
+
+  todos(): Observable<IStock[]> {
+    return this.cliente.get<IStock[]>(this.urlBase + 'todos');
+  }
+  uno(id: number): Observable<IStock> {
+    var stock = new FormData();
+    stock.append('stockId', id.toString());
+    return this.cliente.post<IStock>(this.urlBase + 'uno', stock);
+  }
+
+  insertar(stocks: IStock): Observable<any> {
+    var stock = new FormData();
+    stock.append('productoId', stocks.ProductoId.toString());
+    stock.append('proveedorId',stocks.ProveedorId.toString());
+    stock.append('cantidad',stocks.Cantidad.toString());
+    stock.append('precio_Venta',stocks.Precio_Venta.toString());
+    console.log(stock);
+    return this.cliente.post(this.urlBase + 'insertar', stock);
+
+  }
+  actualizar(stocks: IStock, id:number): Observable<any> {
+    var stock = new FormData();
+    stock.append('stockId', id.toString());
+    stock.append('productoId', stocks.ProductoId.toString());
+    stock.append('proveedorId',stocks.ProveedorId.toString());
+    stock.append('cantidad',stocks.Cantidad.toString());
+    stock.append('precio_Venta',stocks.Precio_Venta.toString());
+    return this.cliente.post(this.urlBase + 'actualizar', stock);
+  }
+  eliminar(id: number): Observable<any> {
+    var stock = new FormData();
+    stock.append('stockId', id.toString());
+    return this.cliente.post(this.urlBase + 'eliminar', stock);
+  }
+
+}
